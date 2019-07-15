ff8098599ffa4989f10a705f44c1189364d4e793
Tushar Kapoor <tushar3.kapoor@in.ey.com>
2019-07-15 14:18:26 +0530

    old script copied

--- get-quorum.sh
+++ get-quorum.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
+
 set -eu -o pipefail
 
-#Installing pakcage wrk
-sudo apt-get install build-essential libssl-dev git -y
-git clone https://github.com/wg/wrk.git wrk
-cd wrk
-make
-# move the executable to somewhere in your PATH, ex:
-sudo cp wrk /usr/local/bin
-
 # install build deps
-add-apt-repository ppa:ethereum/ethereum
+add-apt-repository ppa:ethereum/ethereum -y
 apt-get update
-#apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk
-apt-get install -y build-essential unzip libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner software-properties-common default-jdk maven
+apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk
 
 # install constellation
-#wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.1.0/constellation-0.3.2-ubuntu1604.tar.xz
-#tar -xf constellation-0.3.2-ubuntu1604.tar.xz
-#cp constellation-0.3.2-ubuntu1604/constellation-node /usr/local/bin
-#chmod 0755 /usr/local/bin/constellation-node
-#rm -rf constellation-0.3.2-ubuntu1604*
-
-# install constellation
-CVER="0.3.2"
-CREL="constellation-$CVER-ubuntu1604"
-wget -q https://github.com/jpmorganchase/constellation/releases/download/v$CVER/$CREL.tar.xz
-tar xfJ ${CREL}.tar.xz
-cp ${CREL}/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
-rm -rf ${CREL}
+wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.1.0/constellation-0.1.0-ubuntu1604.tar.xz
+tar -xf constellation-0.1.0-ubuntu1604.tar.xz
+cp constellation-0.1.0-ubuntu1604/constellation-node /usr/local/bin
+chmod 0755 /usr/local/bin/constellation-node
+rm -rf constellation-0.1.0-ubuntu1604*
 
 # install golang
 GOREL=go1.7.3.linux-amd64.tar.gz
@@ -39,7 +23,7 @@ rm -f $GOREL
 PATH=$PATH:/usr/local/go/bin
 echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
-# make/installing quorum
+# make/install quorum
 git clone https://github.com/jpmorganchase/quorum.git
 pushd quorum >/dev/null
 make all
